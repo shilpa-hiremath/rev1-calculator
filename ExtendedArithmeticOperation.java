66da96687d2c16fca111462d5679ec04eeba9bc0
shilpa hiremath <shilpa.hiremath@kletech.ac.in>
2024-07-03 22:16:04 +0530

    Revision of git branch calculator functions

---
+++ ExtendedArithmeticOperation.java
@@ -0,0 +1,17 @@
+public class ExtendedArithmeticOperation {
+
+    public static float SquareRoot(float op1)
+    {
+        return op1 * op1;
+    }
+
+    public static float CubeRoot(float op1)
+    {
+        return op1 * op1  * op1;
+    }
+    public static float modulus(float op1,float op2)
+    {
+        return op1 % op2;
+    }
+    
+}
